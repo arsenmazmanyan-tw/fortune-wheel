2a4dc26ff61c88cc92352dd60265fffe9ad1fae3
arsenmazmanyan-tw <164858088+arsenmazmanyan-tw@users.noreply.github.com>
2024-04-28 14:02:02 +0000

    Deploying to gh-pages from @ arsenmazmanyan-tw/fortune-wheel@dfd09ba6bd3e8d807b256d1ae2707f528496b338 🚀

--- assets/assetsNames/atlas.ts
+++
@@ -1,3 +0,0 @@
-export const atlases: { name: string; json: string; png: string }[] = [
-    { name: 'spinner', json: 'assets/atlas/spinner@1.png.json', png: 'assets/atlas/spinner.png' },
-];
--- assets/assetsNames/audio.ts
+++
@@ -1,4 +0,0 @@
-export const audioAssets: AssetNameAndPath[] = [
-    { name: 'audio', path: 'assets/audio/audio.mp3' },
-    { name: 'sound', path: 'assets/audio/sound.mp3' },
-];
